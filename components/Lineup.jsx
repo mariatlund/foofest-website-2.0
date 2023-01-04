b2543b33848469e0e4c733ba4aa3011a7919361d
Maria Lundqvist <mari28rd@stud.kea.dk>
2023-01-04 17:27:21 +0100

    fetching and displaying lineup, attempting filtering by genre

--- components/Lineup.jsx
+++ components/Lineup.jsx
@@ -1,20 +1,77 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LineupMenu from "./UI-components/LineupMenu";
 import LineupCard from "./UI-components/LineupCard";
 
-function Lineup() {
+function Lineup(props) {
+  // // filter and sort settings
+  // const lineupSettings = {
+  //   filterBy: "all",
+  //   sortBy: "none",
+  //   activeArray: props.lineup,
+  // };
+
+  // // add each band genre to an array
+  // let allGenres = [];
+  // props.lineup.forEach((band) => {
+  //   allGenres.push(band.genre);
+  // });
+  // // remove duplicates
+  // let genres = [];
+  // allGenres.forEach((item) => {
+  //   if (!genres.includes(item)) {
+  //     genres.push(item);
+  //   }
+  // });
+
+  // // FILTERING
+  // function setFilter(event) {
+  //   const filter = event.target.value;
+  //   lineupSettings.filterBy = filter;
+  //   console.log("setFilter:", filter);
+  //   filterList();
+  // }
+
+  // function filterList() {
+  //   let filteredLineup = [];
+  //   props.lineup.forEach((band) => {
+  //     if (band.genre === lineupSettings.filterBy) {
+  //       filteredLineup.push(band);
+  //     }
+  //   });
+  //   lineupSettings.activeArray = filteredLineup;
+  // }
+
   return (
     <section className="lineup">
-      <LineupMenu />
+      <div className="lineup-menu">
+        <h2>Lineup</h2>
+        <div className="lineup-menu-actions">
+          <label htmlFor="filter">Filter</label>
+          <select name="filter" id="filter" onChange={props.setFilter}>
+            <option value="All">All</option>
+            <option value="Rock">Rock</option>
+            <option value="Hard Rock">Hard Rock</option>
+            <option value="Heavy Metal">Heavy Metal</option>
+            <option value="Alternative Rock">Alternative Rock</option>
+            <option value="Alternative Metal">Alternative Metal</option>
+            <option value="Grunge">Grunge</option>
+            <option value="Hardcore Punk">Hardcore Punk</option>
+          </select>
+          <label htmlFor="sort">Sort</label>
+          <select name="sort" id="sort">
+            <option value="alpha">A-Z</option>
+            <option value="metal">Z-A</option>
+          </select>
+        </div>
+      </div>
       <section className="lineup-list">
         <div className="lineup-showing">
           <h4>Bands</h4>
           <h5>Showing all</h5>
         </div>
-        <LineupCard />
-        <LineupCard />
-        <LineupCard />
-        <LineupCard />
+        {props.activeArray.map((band) => (
+          <LineupCard name={band.name} genre={band.genre} />
+        ))}
       </section>
     </section>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,73 @@
 import Splash from "../components/Splash";
 import Lineup from "../components/Lineup";
 import Headliners from "../components/Headliners";
+import { useState } from "react";
 
-export default function Home() {
+export default function Home(props) {
+  const lineup = props.lineupData;
+  // filter and sort settings
+  const [lineupSettings, setLineupSettings] = useState({
+    filterBy: "all",
+    sortBy: "none",
+    activeArray: lineup,
+  });
+
+  // add each band genre to an array
+  let allGenres = [];
+  lineup.forEach((band) => {
+    allGenres.push(band.genre);
+  });
+  // remove duplicates
+  let genres = [];
+  allGenres.forEach((item) => {
+    if (!genres.includes(item)) {
+      genres.push(item);
+    }
+  });
+
+  // FILTERING
+  function setFilter(event) {
+    const filter = event.target.value;
+    setLineupSettings({ ...lineupSettings, filterBy: filter });
+    console.log("setFilter:", filter);
+    filterList();
+  }
+
+  function filterList() {
+    let filteredLineup = [];
+    lineup.forEach((band) => {
+      if (band.genre === lineupSettings.filterBy) {
+        filteredLineup.push(band);
+      }
+    });
+    setLineupSettings({ ...lineupSettings, activeArray: filteredLineup });
+  }
+
   return (
     <>
       <Splash />
       <Headliners />
-      <Lineup />
+      <Lineup lineup={props.lineupData} setFilter={setFilter} activeArray={lineupSettings.activeArray} />
     </>
   );
 }
+
+// FETCHING LINEUP DATA
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:8080/bands");
+  const lineupData = await res.json();
+  return {
+    props: { lineupData },
+  };
+}
+
+/*
+// FETCTHING AREA DATA
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:8080/available-spots");
+  const areaData = await res.json();
+  return {
+    props: { areaData },
+  };
+}
+*/
