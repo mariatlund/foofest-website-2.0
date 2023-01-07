726923a9c8571679b59e4e1d2b4f22c50d87880f
Maria Lundqvist <mari28rd@stud.kea.dk>
2023-01-07 16:12:41 +0100

    text clipping to animated image, media queries need fixing

--- components/Splash.jsx
+++ components/Splash.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Image from "next/image";
+import textImage from "../assets/colour-graphic.png";
 
 function Splash() {
   return (
     <section className="SplashContainer">
       <h1>The best damned festival</h1>
-      <h1>28:13:45:14</h1>
+      {/* <div className="text-bg"></div> */}
+      {/* <h1>28:13:45:14</h1> */}
+
+      <Image src={textImage} className={"textImage"} width={"1164"} height={"1164"} />
     </section>
   );
 }
