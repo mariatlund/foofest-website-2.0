4bac8eba17aaa5831942dc47e66119bb93670d45
Maria Lundqvist <mari28rd@stud.kea.dk>
2023-01-08 13:26:52 +0100

    splash h1 styling updated, animation and media queries working

--- components/Splash.jsx
+++ components/Splash.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Image from "next/image";
 import textImage from "../assets/colour-graphic.png";
+import { useMediaQuery } from "usehooks-ts";
 
 function Splash() {
+  const matches = useMediaQuery("(min-width: 900px)");
+
   return (
     <section className="SplashContainer">
       <h1>The best damned festival</h1>
       {/* <div className="text-bg"></div> */}
       {/* <h1>28:13:45:14</h1> */}
 
-      <Image src={textImage} className={"textImage"} width={"1164"} height={"1164"} />
+      {matches ? <Image src={textImage} className={"bigImage"} width={"1164"} height={"1164"} /> : <Image src={textImage} className={"smallImage"} width={"1164"} height={"1164"} />}
     </section>
   );
 }
