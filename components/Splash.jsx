46671efc23a5cececff6f1a60c65382d8132016d
Maria Lundqvist <mari28rd@stud.kea.dk>
2023-01-09 12:55:42 +0100

    splash media query fix

--- components/Splash.jsx
+++ components/Splash.jsx
@@ -4,7 +4,7 @@ import textImage from "../assets/colour-graphic.png";
 import { useMediaQuery } from "usehooks-ts";
 
 function Splash() {
-  const matches = useMediaQuery("(min-width: 900px)");
+  const matches = useMediaQuery("(max-width: 900px)");
 
   return (
     <section className="SplashContainer">
@@ -12,7 +12,7 @@ function Splash() {
       {/* <div className="text-bg"></div> */}
       {/* <h1>28:13:45:14</h1> */}
 
-      {matches ? <Image src={textImage} className={"bigImage"} width={"1164"} height={"1164"} /> : <Image src={textImage} className={"smallImage"} width={"1164"} height={"1164"} />}
+      {matches ? <Image src={textImage} className={"smallImage"} width={"1164"} height={"1164"} /> : <Image src={textImage} className={"bigImage"} width={"1164"} height={"1164"} />}
     </section>
   );
 }
