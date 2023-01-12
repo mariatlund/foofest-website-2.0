36fbb68e877cfa412c2a2a0fbc4b10885aa8ddcb
Maria Lundqvist <mari28rd@stud.kea.dk>
2023-01-12 16:31:26 +0100

    displaying lineup in festival poster style, media queries for font sizes

--- components/Lineup.jsx
+++ components/Lineup.jsx
@@ -40,38 +40,71 @@ function Lineup(props) {
   //   });
   //   lineupSettings.activeArray = filteredLineup;
   // }
+  let lineupTop = [];
+  lineupTop = props.lineup.slice(3, 14);
+  let lineupMiddle = ["A Perfect Circle", "Tool", "Terminalist", "Raised Fist", "Refused"];
+  let lineupBottom = [];
+  lineupBottom = props.lineup.slice(16, 36);
 
   return (
+    // <section className="lineup">
+    //   <div className="lineup-menu">
+    //     <h2>Lineup</h2>
+    //     <div className="lineup-menu-actions">
+    //       <label htmlFor="filter">Filter</label>
+    //       <select name="filter" id="filter" onChange={props.setFilter}>
+    //         <option value="All">All</option>
+    //         <option value="Rock">Rock</option>
+    //         <option value="Hard Rock">Hard Rock</option>
+    //         <option value="Heavy Metal">Heavy Metal</option>
+    //         <option value="Alternative Rock">Alternative Rock</option>
+    //         <option value="Alternative Metal">Alternative Metal</option>
+    //         <option value="Grunge">Grunge</option>
+    //         <option value="Hardcore Punk">Hardcore Punk</option>
+    //       </select>
+    //       <label htmlFor="sort">Sort</label>
+    //       <select name="sort" id="sort">
+    //         <option value="alpha">A-Z</option>
+    //         <option value="metal">Z-A</option>
+    //       </select>
+    //     </div>
+    //   </div>
+    //   <section className="lineup-list">
+    //     <div className="lineup-showing">
+    //       <h4>Bands</h4>
+    //       <h5>Showing all</h5>
+    //     </div>
+    //     {props.activeArray.map((band) => (
+    //       <LineupCard name={band.name} genre={band.genre} />
+    //     ))}
+    //   </section>
+    // </section>
+
     <section className="lineup">
-      <div className="lineup-menu">
-        <h2>Lineup</h2>
-        <div className="lineup-menu-actions">
-          <label htmlFor="filter">Filter</label>
-          <select name="filter" id="filter" onChange={props.setFilter}>
-            <option value="All">All</option>
-            <option value="Rock">Rock</option>
-            <option value="Hard Rock">Hard Rock</option>
-            <option value="Heavy Metal">Heavy Metal</option>
-            <option value="Alternative Rock">Alternative Rock</option>
-            <option value="Alternative Metal">Alternative Metal</option>
-            <option value="Grunge">Grunge</option>
-            <option value="Hardcore Punk">Hardcore Punk</option>
-          </select>
-          <label htmlFor="sort">Sort</label>
-          <select name="sort" id="sort">
-            <option value="alpha">A-Z</option>
-            <option value="metal">Z-A</option>
-          </select>
-        </div>
+      <div className="lineup-showing">
+        <h4>Lineup</h4>
+        <h5>Featured bands</h5>
       </div>
       <section className="lineup-list">
-        <div className="lineup-showing">
-          <h4>Bands</h4>
-          <h5>Showing all</h5>
+        <div className="primary-bands">
+          {lineupTop.map((band) => (
+            <h2>{band.name}</h2>
+          ))}
         </div>
-        {props.activeArray.map((band) => (
-          <LineupCard name={band.name} genre={band.genre} />
-        ))}
+        <div className="secondary-bands">
+          {lineupMiddle.map((band) => (
+            <h3>{band}</h3>
+          ))}
+        </div>
+        <div className="tertiary-bands">
+          {lineupBottom.map((band) => (
+            <h4>{band.name}</h4>
+          ))}
+        </div>
+
+        {/* {props.activeArray.map((band) => (
+        <LineupCard name={band.name} genre={band.genre} />
+      ))} */}
       </section>
     </section>
   );
